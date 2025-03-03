da2c621a5b75a6fd10baf535071ebe15cc29c6c6
vnbtest <vnb-ns@yandex.ru>
2025-03-03 11:18:16 +0300

    fix install sh

--- test/install_docker_ubuntu.sh
+++ test/install_docker_ubuntu.sh
@@ -3,9 +3,9 @@
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
+sudo install -y -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -17,7 +17,7 @@ echo \
 sudo apt-get update
 
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
 
